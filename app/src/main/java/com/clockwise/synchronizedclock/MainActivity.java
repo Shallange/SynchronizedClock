d8620e44324d009ea1a8352c22daa59aa2a78a79
Jimmy kroneld <jimmy.kroneld@hotmail.com>
2023-10-10 00:28:21 +0200

    Implement button click to update TextClock
    - Added an OnClickListener to the 'Click here to update time' button.
    
    - On button click, the TextClock displays the current date and time in the format 'yyyy-MM-dd_HH-mm-ss'.
    
    - Renamed XML IDs for clarity:
    
      - Changed 	extClock3 to 	extClock1.
    
      - Changed utton2 to utton1.

--- app/src/main/java/com/clockwise/synchronizedclock/MainActivity.java
+++ app/src/main/java/com/clockwise/synchronizedclock/MainActivity.java
@@ -3,6 +3,14 @@ package com.clockwise.synchronizedclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    Button button1 = findViewById(R.id.button1);//"Click here to update time"-button
+    button1.setOnClickListener(new View.OnClickListener() {//adding an eventlistener(OnClick) to button1
+        @Override
+        public void onClick(View v) {
+            String timeStamp = new SimpleDateFormat("yyyy-M-dd_HH-mm-ss").format(Calendar.getInstance().getTime());
+            //finding the textClock id and setting the text to the current date/time
+            TextClock textClock1 = findViewById(R.id.textClock1);
+            textClock1.setText(timeStamp);
+        }
+    });
+
     }
 }
