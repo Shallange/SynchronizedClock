b9ccc7ef00e0e6415425849460335cb673010b9f
Jimmy kroneld <jimmy.kroneld@hotmail.com>
2023-10-24 10:53:08 +0200

    Refactored UdpSntpService to calculate NTP offset, abstracted buffer response parsing.

--- app/src/main/java/com/clockwise/synchronizedclock/UdpSntpService.java
+++ app/src/main/java/com/clockwise/synchronizedclock/UdpSntpService.java
@@ -20,6 +20,49 @@ public class UdpSntpService {
     /** Define a constant for NTP server port */
     private static final int NTP_PORT = 123;
 
+    /** Starting buffer position for original timestamp. 24-27 -- 28-31 */
+    int t1StartBuff = 24;
+
+    /** Starting buffer position for receive timestamp. 32-35 -- 36-39 */
+    int t2StartBuff = 32;
+
+    /** Starting buffer position for transmit timestamp. 40-43 -- 44-47 */
+    int t3StartBuff = 40;
+
+    /** Starting buffer position for reference timestamp. 16-19 -- 20-23 */
+    int t4StartBuff = 16;
+
+    /**
+     * Parse the response buffer to extract NTP timestamps.
+     *
+     * @param buffer The response buffer.
+     * @param b The starting position for the timestamp in the buffer.
+     * @return The extracted timestamp.
+     */
+    private long responseBuffer(byte[] buffer,int b){
+        byte b0 = buffer[b];
+        byte b1 = buffer[b+1];
+        byte b2 = buffer[b+2];
+        byte b3 = buffer[b+3];
+
+        byte b4 = buffer[b+4];
+        byte b5 = buffer[b+5];
+        byte b6 = buffer[b+6];
+        byte b7 = buffer[b+7];
+        long seconds = (b0 & 0xFFL) << 24 |
+                       (b1 & 0xFFL) << 16 |
+                       (b2 & 0xFFL) << 8  |
+                       (b3 & 0xFFL);
+        // Extract the fractional seconds from the response
+        long fraction = (b4 & 0xFFL) << 24 |
+                        (b5 & 0xFFL) << 16 |
+                        (b6 & 0xFFL) << 8  |
+                        (b7 & 0xFFL);
+        //ntp-time is in seconds(January 1,1900) and this converts it to unix which is in milliseconds(January 1, 1970)
+        long timestamp =(seconds - 2208988800L) * 1000L + (fraction * 1000L / 0x100000000L);
+        return timestamp;
+    }
+
     /**
      * Fetches the NTP time from the specified server.
      * @return Unix timestamp representing the NTP time.
@@ -57,26 +100,15 @@ public class UdpSntpService {
             socket.receive(response);
             Log.d("NTP_CLIENT", "Received NTP response");
 
-            // Extract the seconds from the response
-            long seconds = (buffer[40] & 0xFFL) << 24 |
-                           (buffer[41] & 0xFFL) << 16 |
-                           (buffer[42] & 0xFFL) << 8  |
-                           (buffer[43] & 0xFFL);
+            long t1 = responseBuffer(buffer,t1StartBuff);
+            long t2 = responseBuffer(buffer,t2StartBuff);
+            long t3 = responseBuffer(buffer,t3StartBuff);
+            long t4 = responseBuffer(buffer,t4StartBuff);
 
-            // Extract the fractional seconds from the response
-            long fraction = (buffer[44] & 0xFFL) << 24 |
-                            (buffer[45] & 0xFFL) << 16 |
-                            (buffer[46] & 0xFFL) << 8  |
-                            (buffer[47] & 0xFFL);
+            long offset = ((t2 - t1) + (t3 - t4)) / 2;
+          
             Log.d("NTP_CLIENT", "NTP Response bytes: " + Arrays.toString(buffer));
-
-            //ntp-time is in seconds(January 1,1900) and this converts it to unix which is in milliseconds(January 1, 1970)
-            long timestamp =(seconds - 2208988800L) * 1000L + (fraction * 1000L / 0x100000000L);
-
-            //Returns the Unix timestamp
-            Log.d("NTP_CLIENT", "Extracted Unix timestamp: " + timestamp);
-
-            return timestamp;
+            return offset;
             //Catch Socket Exceptions
         } catch (SocketException e) {
             //rethrow the exception as a RuntimeException
